e71886ac58ebc86d54edcf843776045df0a0d181
odik_web <116338011+odik5222@users.noreply.github.com>
2024-08-16 20:28:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//1
+let arr = [10.45, 9.519, 85.56, 22, 6.26, 98.245, 15, 34.2]
+let min = Math.min(...arr)
+let max = Math.max(...arr)
+
+let math = max / min
+
+console.log(max);
+console.log(min);
+console.log(math);
+
+
+//2
+let num1 = 10.4568313123132
+
+num1 = num1.toFixed(3)
+
+console.log('Длина строки:',num1.length,num1);
